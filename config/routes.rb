ccc6cd2f1679fc33eb2777fd30cb3cf685169a35
torvik <pless.victor@gmail.com>
2018-04-25 13:57:56 +0000

    application

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :pins do
+    member do
+      put 'like', to: 'pins#upvote'
+    end
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "pins#index"
+
+end
